4ab687ae13cfcee7778eb7b2094c3886b4c77581
John Gray <john@zippykid.com>
2012-02-19 18:53:11 -0600

    DoneList::find_all

---
+++ lib/done_list.php
@@ -0,0 +1,21 @@
+<?php
+class DoneList {
+  public static function find_all(){
+    self::use_db();
+    $done = array();
+    $q = mysql_query("select * from done_items order by created_at desc");
+
+    while($row = mysql_fetch_assoc($q)){
+      array_push($done, $row);
+    }
+
+    mysql_free_result($q);
+    return $done;
+  }
+
+  private static function use_db(){
+    mysql_connect("mysql-shared-02.phpfog.com", "Slim-29435", "hMVowjX8j9JYQu");
+    mysql_select_db("donelist_phpfogapp_com");
+  }
+}
+?>
